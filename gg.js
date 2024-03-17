022bca94684b3a9f1ccaf0dcb5023578944eb496
Egorbober <tanahu2010@gmail.com>
2024-03-17 20:19:21 +0700

    krestiki_noliki

--- gg.js
+++ gg.js
@@ -4,6 +4,26 @@ let board = [0,0,0,0,0,0,0,0,0];
 let player = 1;
 let winner;
 
+const COMBO = [[0, 1, 2], [3, 4, 5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+function checkWin() {
+    let zeroNums = 0;
+    for (let i = 0; i < board.length; i++) {if (board[i] === 0) zeroNums++}
+    if (zeroNums === 0 ) winner = 'Достойная ничья'
+
+    let p1, p2,p3;
+    for (let indexes of COMBO) {
+        [p1,p2,p3] = indexes
+        if (board[p1] === 1 && board[p2] === 1 && board[p3] === 1) {
+            winner = 'Победил 1 игрок'}
+        else if (board[p1] === 2 && board[p2] === 2 && board[p3] === 2) {
+            winner = 'победил 2 игрок'}
+        }
+    if (winner) {
+        alert(winner)}
+    }
+
+
 function handleCellClick(index){
     let cell = board[index];
     if (cell === 0 && !winner) {
@@ -11,6 +31,7 @@ function handleCellClick(index){
         player = player == 1 ? 2 : 1
         console.log(board)
         render()
+        checkWin()
     }
 }
 
